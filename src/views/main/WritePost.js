902b71c710c962be53052729d064beaf57b2b338
jhk7375 <jhk7375@naver.com>
2020-05-15 08:44:46 +0900

    tag

--- src/views/main/WritePost.js
+++ src/views/main/WritePost.js
@@ -5,6 +5,7 @@ import { ApiAsync } from '../../service/ApiService';
 import InputBase from '@material-ui/core/InputBase';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
+import  Warning from '../../components/alert/Warning';
 
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
@@ -13,7 +14,7 @@ import Divider from '@material-ui/core/Divider';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-
+import Chip from '@material-ui/core/Chip';
 import WriteMenu from '../../components/menu/write/WriteMenu';
 
 import './WritePost.css';
@@ -44,7 +45,10 @@ export default function WritePost() {
     editor: {
       paddingTop: `${theme.spacing(2)}px`,
     },
-
+    chip: {
+      margin: theme.spacing(0.5),
+      color: theme.palette.secondary.textColor
+    },
   }));
 
   // eslint-disable-next-line
@@ -56,10 +60,39 @@ export default function WritePost() {
 
   }
 
+  const [chipData, setChipData] = React.useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [message, setMessage] = React.useState("");
+
+
+  const handleDelete = (chipToDelete) => () => {
+    setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+  };
+
+  const handleInsert = (event) => {
+    if (event.key === 'Enter') {
+      const tag = document.getElementById('tag');
+      const label = tag.value;
+      tag.value = "";
+
+      if(chipData.some((chip) => chip.key === label)){
+        setMessage("이미 입력 하였습니다.");
+        setOpen(true);
+        return;
+      }
+
+      setChipData((chips) => [...chips, {key: label, label: label }]);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
 
   return (
     <Container maxWidth="lg" className={classes.container}>
+      <Warning open={open} onClose={handleClose} message={message} />
       
       <Grid container spacing={1}>
 
@@ -81,11 +114,33 @@ export default function WritePost() {
             <Divider/>
 
             <Paper elevation={0} className={classes.paper}>
+
+              {
+                chipData.map((data) => {
+                  return (
+                      <Chip
+                        key={data.key}
+                        label={data.label}
+                        onDelete={handleDelete(data)}
+                        className={classes.chip}
+                      />
+                  );
+                })
+              }
+
+            </Paper>
+
+
+            <Paper elevation={0} className={classes.paper}>
               <InputBase 
                 className={classes.input}
-                id="subject" 
+                id="tag" 
                 placeholder="태그를 입력하세요."
                 fullWidth 
+                onKeyPress={(event) =>{
+                  handleInsert(event)
+                }}
+  
                 variant="filled"/>
             </Paper>
 
