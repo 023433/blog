c6b23ae29396bc0c81b01df99a6daf26d1662b11
jhk7375 <jhk7375@naver.com>
2020-05-14 22:43:57 +0900

    signin

--- src/views/main/Signin.js
+++ src/views/main/Signin.js
@@ -4,8 +4,6 @@ import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -111,10 +109,7 @@ export default function SignIn() {
             id="password"
             autoComplete="current-password"
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
+     
           <Button
             type="button"
             fullWidth
@@ -126,12 +121,7 @@ export default function SignIn() {
             Sign In
           </Button>
           <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
+            <Grid item xs={12} className={classes.paper}>
               <Link href="#" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
